4ac29e8983a541761d93871c93264e925642a34c
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-11-14 22:59:29 -0300

    initial commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
