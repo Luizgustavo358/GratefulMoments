db36e2937676fefccf12927e15faeccf30099035
Luizgustavo358 <luiz.gustavo358@gmail.com>
2025-12-23 23:32:07 -0300

    creating a unit test to test a streak calculation

---
+++ GratefulMomentsTests/StreakCalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  GratefulMomentsTests.swift
+//  GratefulMomentsTests
+//
+//  Created by Luiz Gustavo Bragança dos Santos on 23/12/25.
+//
+
+import Testing
+
+struct GratefulMomentsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
